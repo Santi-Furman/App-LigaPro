b7ae320a129d374dec736c68add3e5a2d2b4d993
“Santi <santifurman@gmail.com>
2026-08-22 00:27:24 -0300

    fix: PWA image

--- fixture-malvin/vite.config.js
+++ fixture-malvin/vite.config.js
@@ -18,19 +18,19 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'LogoSportivo.png',
+            src: 'LogoSportivo.jpeg',
             sizes: '192x192',
-            type: 'image/png',
+            type: 'image/jpeg',
           },
           {
-            src: 'LogoSportivo.png',
+            src: 'LogoSportivo.jpeg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/jpeg',
           },
           {
-            src: 'LogoSportivo.png',
+            src: 'LogoSportivo.jpeg',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/jpeg',
             purpose: 'maskable',
           },
         ],
